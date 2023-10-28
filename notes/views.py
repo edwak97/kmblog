2d832b7718acc732c5af5e1b192b43e5fa493229
Eduard K <>
2023-10-29 01:36:32 +0300

    templates?

--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from .models import *
+from django.template.loader import render_to_string
+
 #Views
 def printNotes(request):
     return HttpResponse("There are supposed to be notes")
 def printNote(request, id:int):
     note = NoteRecord.objects.get(id=id)
-    return HttpResponse(f"{note.header}<br>{note.body}")
+    return render(request, 'notes/note.html', context={'header':note.header, 'body':note.body})
 #methods
 
