b85fe894948c04a9c36ea35c66b027cea426a5d8
Eduard K <>
2023-11-18 00:30:09 +0300

    Slightly parametrized with JTL + static files

--- notes/models.py
+++ notes/models.py
@@ -7,3 +7,5 @@ class NoteRecord(models.Model):
     body = models.CharField(max_length=4096) #hello, Telegram
     release_date = models.DateField(auto_now_add=True)
     modified_date = models.DateField(auto_now=True)
+    access_show = models.PositiveSmallIntegerField(default=0)
+    access_comment = models.PositiveSmallIntegerField(default=0)
--- notes/views.py
+++ notes/views.py
@@ -2,12 +2,18 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from .models import *
 from django.template.loader import render_to_string
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
 
 #Views
 def printNotes(request):
-    return HttpResponse("There are supposed to be notes")
+    return render(request, 'notes/notes.html', context = {'notesItems': list(NoteRecord.objects.all())})
+   # return HttpResponse("There are supposed to be notes")
 def printNote(request, id:int):
-    note = NoteRecord.objects.get(id=id)
-    return render(request, 'notes/note.html', context={'header':note.header, 'body':note.body})
+    try:
+        note = NoteRecord.objects.get(id=id)
+        return render(request, 'notes/note.html', context={'header':note.header, 'body':note.body})
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound('Note nod found')
 #methods
 
