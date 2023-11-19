20f2d9c4998351cc9ec6d34f5ae7de8c0a199251
Eduard K <>
2023-11-20 00:39:26 +0300

    + global static files

--- kmblog/settings.py
+++ kmblog/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# for global static files
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
