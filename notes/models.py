9118caabb81c5e6770ef51b181c2728f778ad65b
Eduard K <>
2023-11-23 22:38:44 +0300

    +Notes are in admin panel; +header has to be unique

--- notes/models.py
+++ notes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class NoteRecord(models.Model):
-    header = models.CharField(max_length=100)
+    header = models.CharField(max_length=10, unique=True)
     body = models.CharField(max_length=4096) #hello, Telegram
     release_date = models.DateField(auto_now_add=True)
     modified_date = models.DateField(auto_now=True)
